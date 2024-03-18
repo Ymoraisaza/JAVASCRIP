818280a72f0192685a36941b52a49e94be23c5df
Mora <yojanmora@gmail.com>
2024-03-18 12:01:44 -0500

    proyecto js

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+let nombreUsuario;
+
+function pedirNombre() {
+    nombreUsuario = prompt("¡Bienvenido/a a SF RECORDS Por favor ingresa tu nombre:");
+    if (nombreUsuario == null || nombreUsuario == "") {
+        pedirNombre();
+    }
+}
+
+pedirNombre();
+
+let opcionesProducto = {
+    1: {
+        nombre: "microfonos",
+        precio: 500
+    },
+    2: {
+        nombre: "controlador midi",
+        precio: 750
+    },
+    3: {
+        nombre: "speakers",
+        precio: 850
+    }
+};
+
+let tipoDeProducto;
+
+function pedirTipoDeProducto() {
+    let mensajeOpciones = "seleccione articulo de interes:\n\n";
+    for (let opcion in opcionesProducto) {
+        mensajeOpciones += opcion + ". " + opcionesProducto[opcion].nombre + " ($" + opcionesProducto[opcion].precio + " USD)\n";
+    }
+    tipoDeProducto = prompt(mensajeOpciones);
+    if (!(tipoDeProducto in opcionesProducto)) {
+        alert("Opción de Producto inválida. Por favor, vuelva a intentarlo.");
+        pedirTipoDeProducto();
+    }
+}
+
+pedirTipoDeProducto();
+
+let cantidadProducto = {};
+
+function pedirCantidadProducto() {
+    for (let opcion in opcionesProducto) {
+        let cantidad = prompt("¿Cuántos Productos de " + opcionesProducto[opcion].nombre + " desea?");
+        cantidad = parseInt(cantidad);
+        if (isNaN(cantidad)) {
+            alert("Lo siento, cantidad inválida. Por favor, vuelva a intentarlo.");
+            pedirCantidadProducto();
+            break;
+        } else {
+            cantidadProducto[opcion] = cantidad;
+        }
+    }
+}
+
+pedirCantidadProducto();
+
+let precioTotal = 0;
+for (let opcion in opcionesProducto) {
+    precioTotal += opcionesProducto[opcion].precio * cantidadProducto[opcion];
+}
+
+alert("¡Gracias, " + nombreUsuario + "! El precio total de los Productos es de $" + precioTotal + " USD.");
